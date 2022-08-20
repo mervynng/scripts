4830ffb44511d5b3b79193fe7790b2cfe16a5796
mervynng <ng.mervyn@gmail.com>
2022-08-20 05:15:55 +0000

    Include unmodified Password strength code

---
+++ portfolio/week2/menu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This scripts firstly checks that the input password matches the stored password, then provides menu options to:
+# 1. Create a folder
+# 2. Copy a folder
+# 3. Set a Password
+#
+# Author: Mervyn Ng
+# 20 Aug 2022 - 1st version
+
+./passwordCheck.sh
+
+# Debugging variable
+# echo $? 
