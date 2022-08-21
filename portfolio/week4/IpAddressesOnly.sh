9d8f9e5a9e59b27529d2b97ab3805109e692cbf8
mervynng <ng.mervyn@gmail.com>
2022-08-21 12:19:04 +0000

    Commit Sed IpAddressOnly.sh

--- portfolio/week4/IpAddressesOnly.sh
+++ portfolio/week4/IpAddressesOnly.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # A sed use case to filter out lines
-# 
+# Author: Mervyn Ng
+# Date: 21 Aug 22
 # 
 
 
@@ -15,24 +16,30 @@ net_info="$(ifconfig)"
 addresses=$(echo "$net_info" | sed -n '/inet / { 
       
 s/inet/IP Address:/ 
-      
+
+#Capitalises Subnet Mask, and inserts new line
+
 s/netmask/\n\t\tSubnet Mask:/ 
-      
+
+#Capitalises broadcast, and inserts new line
+
 s/broadcast/\n\t\tBroadcast Address:/ 
       
 p
-      
-}')
-
-IpAddress=$(echo "$addresses" | sed -n '/IP Address / { 
-
-p
 
 }')
 
+#Assign new variable
 
-#format output 
-      
-echo -e "IP addresses on this computer are:\n$IpAddress" 
-     
-					
+#test echo
+
+IpAddress=$(echo "$addresses" | sed -n '/IP Address/{!d;p}')
+
+#format output
+
+echo -e "IP addresses on this computer are:\n$IpAddress"
+
+exit 0
+
+# References:
+# R. MCKNIGHT (2022) Write scripts that can parse text in a meaningful way. https://courses.ecu.edu.au/courses/13892/pages/4-dot-5-write-scripts-that-can-parse-text-in-a-meaningful-way?module_item_id=955323
