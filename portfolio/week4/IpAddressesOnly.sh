788f259f063df1cc483e46112edc9980e80ebf3a
mervynng <ng.mervyn@gmail.com>
2022-08-21 09:23:35 +0000

    Commit for IpAddresses for week 4

---
+++ portfolio/week4/IpAddressesOnly.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# A sed use case to filter out lines
+# 
+# 
+
+
+
+#!/bin/bash      
+#get info about networking from the ifconfig command 
+      
+net_info="$(ifconfig)" 
+      
+#parse out the ip address lines using sed 
+      
+addresses=$(echo "$net_info" | sed -n '/inet / { 
+      
+s/inet/IP Address:/ 
+      
+s/netmask/\n\t\tSubnet Mask:/ 
+      
+s/broadcast/\n\t\tBroadcast Address:/ 
+      
+p
+      
+}')
+
+IpAddress=$(echo "$addresses" | sed -n '/IP Address / { 
+
+p
+
+}')
+
+
+#format output 
+      
+echo -e "IP addresses on this computer are:\n$IpAddress" 
+     
+					
