ae78cd0a716c87d84cb77f9ddc64702997243d0a
mervynng <ng.mervyn@gmail.com>
2022-08-22 00:50:32 +0000

    Commit and finalise week 1-2

--- portfolio/week2/foldermaker.sh
+++ portfolio/week2/foldermaker.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
 # ECU Script Week 2
 # This script makes folders with the mkdir command
-#
+# Author: Mervyn Ng
+# Original Date: 02 Feb 2022
 # Edit 1 - added space for readability 20 Aug 22
-#!/bin/bash
+
+# Prompts the user with a predefined script. And saves the variable into folderName
+# THe user is able to read what they type
 
 read -p "type the name of the folder you would like to create " folderName
 
+# Makes a directory which is entered by the user
+
 mkdir "$folderName"
+
+exit 0
+
+# References
+# R. MCKNIGHT (2022). Understanding and executing scripts that require interactivity. https://courses.ecu.edu.au/groups/15209/discussion_topics/142127?module_item_id=955301
